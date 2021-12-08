85325b55a4268a24686070bcbabd07c70277b9d2
Muhamed Didovic <muhamed.didovic@gmail.com>
2021-12-08 14:19:34 +0100

    createFolder module

---
+++ src/create/createFolder.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+
+function createFolder(downloadFolder) {
+
+  const sep = path.sep;
+  const initDir = path.isAbsolute(downloadFolder) ? sep : '';
+
+  downloadFolder.split(sep).reduce((parentDir, childDir) => {
+    const curDir = path.resolve(parentDir, childDir);
+    try {
+      fs.mkdirSync(curDir);
+    } catch (err) {
+      // console.log('ERRROR', err);
+      if (err.code !== 'EEXIST' && err.code !== 'EISDIR') {
+        throw err;
+      }
+      console.log('curDir', curDir, makeDownloadFolderPath(downloadFolder));
+      if (curDir == makeDownloadFolderPath(downloadFolder)){
+        console.log(`Directory ${curDir} already exists`.blue);
+      }
+
+      return curDir;
+    }
+    console.log(`Directory ${curDir} created`.blue);
+    return curDir;
+  }, initDir);
+  console.log('--------------');
+}
+
+function makeDownloadFolderPath(downloadFolder) {
+  const sep = path.sep;
+  const folders = process.argv[1].split(sep);
+  folders.pop();
+  folders.push(downloadFolder);
+  const downloadFolderPath = folders.join(sep);
+  return downloadFolderPath;
+}
+
+module.exports = createFolder;
