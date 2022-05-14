786c90933a5d77b271088067eb7afbfa480f7e34
Muhamed Didovic <muhamed.didovic@gmail.com>
2022-05-14 20:30:18 +0200

    update version

--- runner.js
+++ runner.js
@@ -3,6 +3,7 @@ const downloader = require('./index');
 (async () => {
   console.time('took');
   let prompt = await downloader.prompt()
+  console.log('inputs', prompt);
   await downloader.run(prompt)
   console.timeEnd('took');
 })()
