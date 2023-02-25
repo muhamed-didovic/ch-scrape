f4201d1f8fd5f1a1f61feea7cb6360e904593bbe
Muhamed Didovic <muhamed.didovic@gmail.com>
2023-02-25 07:14:16 +0100

    update login to comply with new url and login system

--- src/getToken.js
+++ src/getToken.js
@@ -1,19 +1,39 @@
-const axios = require('axios');
+// const axios = require('axios');
+const req = require('requestretry');
+const j = req.jar();
+const request = req.defaults({ jar: j, retryDelay: 500, fullResponse: true });
 
-const getToken = async (e_mail, password) => {
+const getToken = async (email, password) => {
   try {
-    let res = await axios({
-      url    : 'https://coursehunter.net/api/auth/login',
-      method : 'put',
+    /*let res = await axios({
+      url    : 'https://coursehunter.net/sign-in',
+      // method : 'POST',
+      // maxRedirects: 0,
+      // maxRedirects: 0,
+      // redirect: 'manual',
       headers: {
-        'content-type'               : 'application/json',
-        'Access-Control-Allow-Origin': '*'
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*!/!*;q=0.8,application/signed-exchange;v=b3;q=0.7",
       },
-      data   : JSON.stringify({ e_mail: e_mail, password: password }),
+      // "method": "POST",
+      // "mode": "cors",
+      // "credentials": "include",
+      data
+    })*/
+
+    const res = await request.post({
+      url            : 'https://coursehunter.net/sign-in',
+      throwHttpErrors: false,
+      followRedirect : true,
+      headers        : {
+        'content-type': 'application/json',
+      },
+      body           : JSON.stringify({ email, password }),
+      verify         : false
     })
 
-    if (!res.data.token) throw new Error('not token from response')
-    return res.headers['set-cookie'][1] + '; accessToken=' + res.data.token;
+    // console.log('headers', res.status, res.headers);
+    let [xsrfToken,] = res.headers['set-cookie']
+    return xsrfToken.split(';')[0]; // accessToken=' + res.data.token;
   } catch (error) {
     throw new Error(error.response.data);
   }
